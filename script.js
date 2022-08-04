bf401b046bca96a6e804cbbf5157a650924d2335
Henry Gabriel <96191361+HenryGabriell@users.noreply.github.com>
2022-08-04 11:38:01 -0300

    imagens sem fundo

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', function(){
     var header = document.querySelector('header');
     header.classList.toggle('sticky',window.scrollY > 0);
 });
-
+//navbar responsiva
 function toggleMenu(){
     var menuToggle = document.querySelector('.toggle');
     var menu= document.querySelector('.menu');
